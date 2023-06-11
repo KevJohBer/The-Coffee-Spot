482c31b7295188b55a4f14086ce3e37eabba6b57
KevJohBer <kevin.bergstrom0313@gmail.com>
2023-06-11 19:00:11 +0000

    tweak requirements.txt to fix deploy failure

--- profiles/models.py
+++ profiles/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_address = models.CharField(max_length=40, null=True, blank=True)
-    default_postal_code = models.IntegerField(range(10000, 99999))
+    default_postal_code = models.IntegerField(range(10000, 99999), null=True)
     default_city = models.CharField(max_length=40, null=True, blank=True)
     avatar = models.ImageField(upload_to=None, default='no image found')
 
