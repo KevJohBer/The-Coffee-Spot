29bdaf90b0b4aa35a7eec2dcb26a5f04fe2cdaad
KevJohBer <kevin.bergstrom0313@gmail.com>
2023-06-12 09:30:26 +0000

    fix typo in settings.py

--- the_coffee_spot/settings.py
+++ the_coffee_spot/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage'
+    'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary'
+    'cloudinary',
     'django.contrib.sites',
     'django_countries',
     'allauth',
