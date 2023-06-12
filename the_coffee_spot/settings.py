14b7f204024b07670d4568f6cfb9a95470a07e86
KevJohBer <kevin.bergstrom0313@gmail.com>
2023-06-12 09:25:48 +0000

    link static and mediafiles to cloudinary

--- the_coffee_spot/settings.py
+++ the_coffee_spot/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage'
     'django.contrib.staticfiles',
+    'cloudinary'
     'django.contrib.sites',
     'django_countries',
     'allauth',
@@ -48,7 +50,7 @@ INSTALLED_APPS = [
     'subscription',
     'order',
     'profiles',
-    'crispy_forms'
+    'crispy_forms',
 ]
 
 SITE_ID = 1
@@ -145,8 +147,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = 'media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
