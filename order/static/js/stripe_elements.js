799cff4f142a38d42b16fb77f2da4bcbced4aeb2
KevJohBer <kevin.bergstrom0313@gmail.com>
2023-06-15 08:54:46 +0000

    fix stripe payment intent bug

--- order/static/js/stripe_elements.js
+++ order/static/js/stripe_elements.js
@@ -23,29 +23,32 @@ card.mount('#card-element');
 
 // Form submit handler
 var form = document.getElementById('payment-form');
+var total = document.getElementById('total').value;
 
-form.addEventListener('submit', function(ev) {
-    ev.preventDefault();
-    card.update({'disabled': true});
-    $('#submit-button').attr('disabled', true)
-    stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-        }
-    }).then(function(result) {
-        if (result.error) {
-            var errorDiv = document.getElementById('card-errors');
-            var html = `
-                <span class="icon" role="alert">
-                    <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>`;
-            $(errorDiv).html(html);
-            card.update({'disabled': false});
-        } else {
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit()
+if (total > 0) {
+    form.addEventListener('submit', function(ev) {
+        ev.preventDefault();
+        card.update({'disabled': true});
+        $('#submit-button').attr('disabled', true)
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
             }
-        }
+        }).then(function(result) {
+            if (result.error) {
+                var errorDiv = document.getElementById('card-errors');
+                var html = `
+                    <span class="icon" role="alert">
+                        <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+                $(errorDiv).html(html);
+                card.update({'disabled': false});
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit()
+                }
+            }
+        });
     });
-});
+}
