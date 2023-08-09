cef7392a50e62dc6068a98181c181d92a374eb68
KevJohBer <kevin.bergstrom0313@gmail.com>
2023-08-09 10:34:17 +0000

    add 500 and 403 templates

--- the_coffee_spot/views.py
+++ the_coffee_spot/views.py
@@ -14,10 +14,9 @@ def handler404(request, *args, **kwargs):
 
 def handler403(request, *args, **kwargs):
     """ Error Handler 403 - page Not Found """
-    return render(request, "errors/404.html", status=403)
+    return render(request, "errors/403.html", status=403)
 
 
 def handler500(request, *args, **kwargs):
     """ Error Handler 500 - page Not Found """
-    return render(request, "errors/404.html", status=500)
-
+    return render(request, "errors/500.html", status=500)
